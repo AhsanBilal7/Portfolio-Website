685c2a72fcbddd2c24dd0101de9be3cd82fd0cab
AhsanBilal7 <abilal.bee20seecs@seecs.edu.pk>
2023-09-06 16:02:46 +0500

    Correct Mobile Navbar

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -6,8 +6,21 @@ import Navbar from "react-bootstrap/Navbar";
 import navstyle from "../styles/Navbar.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import hamicon from "../images/hamburger.png";
+import crossicon from "../images/close.png";
 function NavbarComp() {
   const router = useRouter();
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [icondis, seticondis] = useState(true);
+  const [crossdis, setcrossdis] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+    seticondis(!icondis);
+    setcrossdis(!crossdis);
+  };
 
   return (
     <>
@@ -15,20 +28,112 @@ function NavbarComp() {
         data-bs-theme="light"
         className={`${navstyle.main} "justify-content-evenly bg-white"`}
       >
-        <Container className={navstyle.container}>
-
-
+        <Container
+          style={{ alignItems: "flex-start" }}
+          className={`${navstyle.mobcontainer}`}
+        >
           <Link href="/" passHref>
             <div className={navstyle.logoImage}>
-              <Image alt="Logo" src={logo} width={200}/>
+              <Image alt="Logo" src={logo} width={200} />
             </div>
           </Link>
 
+          {/* ----------------------------------------------------------- */}
 
+          {/* Add a Navbar component for mobile view */}
+          <Navbar expand="md" className="d-md-none">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse
+              className="d-flex flex-column"
+              id="basic-navbar-nav"
+            >
+              {icondis && (
+                <div>
+                  <Image
+                    src={hamicon}
+                    alt="icon"
+                    width={30}
+                    height={30}
+                    onClick={toggleMenu}
+                  />
+                </div>
+              )}
+              {crossdis && (
+                <div>
+                  <Image
+                    src={crossicon}
+                    alt="icon"
+                    width={30}
+                    height={30}
+                    onClick={toggleMenu}
+                  />
+                </div>
+              )}
+              {menuVisible && (
+                <div className={navstyle.divlinks}>
+                  <Nav className="flex-column">
+                    <Link href="/" passHref>
+                      <Nav.Link
+                        className={
+                          router.pathname === "/" ? navstyle.activeLink : ""
+                        }
+                      >
+                        Home
+                      </Nav.Link>
+                    </Link>
+                    <Link href="/projects" passHref>
+                      <Nav.Link
+                        className={
+                          router.pathname === "/projects"
+                            ? navstyle.activeLink
+                            : ""
+                        }
+                      >
+                        Projects
+                      </Nav.Link>
+                    </Link>
+                    <Link href="/education" passHref>
+                      <Nav.Link
+                        className={
+                          router.pathname === "/education"
+                            ? navstyle.activeLink
+                            : ""
+                        }
+                      >
+                        Education
+                      </Nav.Link>
+                    </Link>
+                    <Link href="/experience" passHref>
+                      <Nav.Link
+                        className={
+                          router.pathname === "/experience"
+                            ? navstyle.activeLink
+                            : ""
+                        }
+                      >
+                        Experience
+                      </Nav.Link>
+                    </Link>
+                    <Link href="/contactUs" passHref>
+                      <Nav.Link
+                        className={
+                          router.pathname === "/contactUs"
+                            ? navstyle.activeLink
+                            : ""
+                        }
+                      >
+                        Contact Us
+                      </Nav.Link>
+                    </Link>
+                  </Nav>
+                </div>
+              )}
+            </Navbar.Collapse>
+          </Navbar>
+          {/* ----------------------------------------------------------- */}
 
-          <nav className="d-flex flex-wrap">
-
-{/* ////////////////////////////////// */}
+          <nav className="d-none d-md-flex flex-wrap">
+            {/* ////////////////////////////////// */}
             <Link href="/" passHref>
               <p
                 className={`${navstyle.link} ${
@@ -38,9 +143,9 @@ function NavbarComp() {
                 Home
               </p>
             </Link>
-{/* ////////////////////////////////// */}
+            {/* ////////////////////////////////// */}
             <Link href="/projects" passHref>
-              <p 
+              <p
                 className={`${navstyle.link} ${
                   router.pathname === "/projects" ? navstyle.activeLink : ""
                 }`}
@@ -49,9 +154,9 @@ function NavbarComp() {
                 Projects
               </p>
             </Link>
-{/* ////////////////////////////////// */}
+            {/* ////////////////////////////////// */}
             <Link href="/education" passHref>
-              <p 
+              <p
                 className={`${navstyle.link} ${
                   router.pathname === "/education" ? navstyle.activeLink : ""
                 }`}
@@ -60,9 +165,9 @@ function NavbarComp() {
                 Education
               </p>
             </Link>
-{/* ////////////////////////////////// */}
+            {/* ////////////////////////////////// */}
             <Link href="/experience" passHref>
-              <p 
+              <p
                 className={`${navstyle.link} ${
                   router.pathname === "/experience" ? navstyle.activeLink : ""
                 }`}
@@ -71,9 +176,9 @@ function NavbarComp() {
                 Experience
               </p>
             </Link>
-{/* ////////////////////////////////// */}
+            {/* ////////////////////////////////// */}
             <Link href="/contactUs" passHref>
-              <p 
+              <p
                 className={`${navstyle.link} ${
                   router.pathname === "/contactUs" ? navstyle.activeLink : ""
                 }`}
@@ -82,8 +187,7 @@ function NavbarComp() {
                 Contact Us
               </p>
             </Link>
-{/* ////////////////////////////////// */}
-
+            {/* ////////////////////////////////// */}
           </nav>
         </Container>
       </Navbar>
